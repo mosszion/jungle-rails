6c076d9befd2a63f4dbda1fbe708d5bf1d519a70
Mussie <musdzion@gmail.com>
2024-08-18 18:01:44 -0400

    add product details file and put the product details tests there

--- cypress/e2e/product_details.cy.js
+++ cypress/e2e/product_details.cy.js
@@ -1,10 +1,13 @@
 describe('Product Details', () => {
-  it('should visit the home page', () => {
-    cy.visit('http://localhost:3000');
+  it('should visit the Products page', () => {
+    cy.visit('http://localhost:3000/products/index');
+
+    // click on a product link to check if it is working
+    cy.get('article').first().click(); 
+
+    // verify the product detai page loads
+    cy.url().should('include', '/products/'); 
   
 });
-it('should visit the home page', () => {
-  cy.visit('http://localhost:3000');
 
-});
 });
