dea19d75e2d7bb01330320bcfa73ca06a57268a9
Mussie <musdzion@gmail.com>
2024-08-18 09:46:08 -0400

    start a new cypress test file

---
+++ cypress/e2e/product_details.cy.js
@@ -0,0 +1,10 @@
+describe('Product Details', () => {
+  it('should visit the home page', () => {
+    cy.visit('http://localhost:3000');
+  
+});
+it('should visit the home page', () => {
+  cy.visit('http://localhost:3000');
+
+});
+});
